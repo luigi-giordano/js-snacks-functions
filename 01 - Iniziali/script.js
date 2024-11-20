41d16f372180ddbec7166f7a617135624da89793
luigi-giordano <luigi.giordano1992@gmail.com>
2024-11-20 16:32:22 +0100

    first commit

---
+++ 01 - Iniziali/script.js
@@ -0,0 +1,15 @@
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+
+const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+
+
+// Dichiara la funzione qui.
+function newArrray(names){
+  return names === names.split('').indexOf().join('')
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+
+
+
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
